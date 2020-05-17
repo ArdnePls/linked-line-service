41e9218bbc350435283c8ee584c780ae07bb3ba9
ArdnePls <andreaceltich97@hotmail.com>
2020-05-17 03:11:07 -0300

    added put to update line and generate old data again

--- app/controllers/fileHandler.js
+++ app/controllers/fileHandler.js
@@ -1,16 +1,20 @@
 const fs = require('fs');
 const { FILE_URL } = require('../config/environment');
-const { getSentenceFromLastLine, obtainLastLine } = require('../helpers/sentenceGenerator');
+const { getSentenceFromLastLine, obtainLastLine, getNewGeneratedData } = require('../helpers/sentenceGenerator');
 
 exports.readFile = (req, res, next) => res.send(fs.readFileSync(FILE_URL, 'utf8'));
 
-// add logic by req index and sentence
-exports.replaceLine = (req, res, next) => res.status(200).send();
+exports.replaceLine = async (req, res, next) => {
+    const { sentence, index } = req.body;
+    const dataToWrite = await getNewGeneratedData(sentence, index);
+    fs.writeFileSync(FILE_URL, dataToWrite);
+    return res.status(200).send();
+}
 
 exports.writeNewLine = async (req, res, next) => {
     const { sentence } = req.body;
     const lastLine = await obtainLastLine();
-    getSentenceFromLastLine(lastLine, sentence)
-        .then(sentenceToWrite => fs.appendFileSync(FILE_URL, sentenceToWrite));
+    const sentenceToWrite = `${getSentenceFromLastLine(lastLine, sentence)}\n`;
+    fs.appendFileSync(FILE_URL, sentenceToWrite);
     return res.status(201).send();
 }
--- app/helpers/sentenceGenerator.js
+++ app/helpers/sentenceGenerator.js
@@ -25,19 +25,42 @@ const createHashForFirstLine = () => {
     return newHash.replace(newHash.substring(0, 2), '00');
 };
 
-exports.obtainLastLine = async () => {
+const obtainAllLines = async () => {
     const readFile = util.promisify(fs.readFile);
     const sentences = await readFile(FILE_URL)
         .then(data => data.toString().split('\n'));
     sentences.pop();
-    console.log(sentences);
+    return sentences;
+}
+
+const obtainLastLine = async () => {
+    const sentences = await obtainAllLines();
     return sentences[sentences.length - 1];
 };
 
-exports.getSentenceFromLastLine = (lastline, sentenceFromRequest) => {
+const getSentenceFromLastLine = (lastline, sentenceFromRequest) => {
     const prevHash = !lastline ? 
         createHashForFirstLine() :
         crypto.createHash('sha256').update(lastline).digest('hex');
     const nonce = calculateNonce(`${prevHash},${sentenceFromRequest},`);
-    return Promise.resolve(`${prevHash},${sentenceFromRequest},${nonce}\n`);
+    return (`${prevHash},${sentenceFromRequest},${nonce}`);
 };
+
+const regenerateDataWhenAlterFile = (sentencesToPersist, sentencesToModificate) => {
+    sentencesToModificate.forEach(sentence => {
+        const sentenceToPersist = sentence.split(',')[1];
+        sentencesToPersist.push(getSentenceFromLastLine(sentencesToPersist[sentencesToPersist.length - 1], sentenceToPersist));
+    });
+    return (`${sentencesToPersist.join('\n')}\n`);
+};
+
+const getNewGeneratedData = async (newSentence, index) => {
+    const sentences = await obtainAllLines();
+    const positionToUpdate = index - 1;
+    const sentencesToPersist = sentences.slice(0, positionToUpdate);
+    sentencesToPersist.push(getSentenceFromLastLine(sentences[positionToUpdate - 1], newSentence));
+    const sentencesToModificate = sentences.slice(index, sentences.length);
+    return regenerateDataWhenAlterFile(sentencesToPersist, sentencesToModificate);
+};
+
+module.exports = { obtainLastLine, getSentenceFromLastLine, getNewGeneratedData, obtainAllLines };
