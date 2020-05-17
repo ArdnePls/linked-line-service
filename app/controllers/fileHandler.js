de95fe1bb16222cf5c288d67e77e625c864d43ba
ArdnePls <andreaceltich97@hotmail.com>
2020-05-17 14:15:36 -0300

    added file reading to response on put and post

--- app/controllers/fileHandler.js
+++ app/controllers/fileHandler.js
@@ -8,7 +8,7 @@ exports.replaceLine = async (req, res, next) => {
     const { sentence, index } = req.body;
     const dataToWrite = await getNewGeneratedData(sentence, index);
     fs.writeFileSync(FILE_URL, dataToWrite);
-    return res.status(200).send();
+    return res.status(200).send(fs.readFileSync(FILE_URL, 'utf8'));
 }
 
 exports.writeNewLine = async (req, res, next) => {
@@ -16,5 +16,5 @@ exports.writeNewLine = async (req, res, next) => {
     const lastLine = await obtainLastLine();
     const sentenceToWrite = `${getSentenceFromLastLine(lastLine, sentence)}\n`;
     fs.appendFileSync(FILE_URL, sentenceToWrite);
-    return res.status(201).send();
+    return res.status(201).send(fs.readFileSync(FILE_URL, 'utf8'));
 }
